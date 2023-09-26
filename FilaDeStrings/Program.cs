0b8e6d7157953a5a5f6de3bccd128bf43c28a220
Paulo Ricardo Sanches <sanchespaulo55@gmail.com>
2023-09-26 09:51:48 -0300

    Fila de strings

---
+++ FilaDeStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> filaDeStrings = new Queue<string>();
+
+        filaDeStrings.Enqueue("Santos");
+        filaDeStrings.Enqueue("Gremio");
+        filaDeStrings.Enqueue("Inter");
+
+        string proximoElemento = filaDeStrings.Peek();
+        Console.WriteLine($"Próximo elemento da fila: {proximoElemento}");
+
+        Console.WriteLine("Itens na fila:");
+        foreach (var item in filaDeStrings)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
